159d59e4d2ace35dad24811cdcf046a3785fa6fa
Caitlyn Chung <caitlyn.chung@gmail.com>
2018-06-15 10:59:16 -0700

    Firebase setup including updating .gitignore

--- plant-buddy/src/index.js
+++ plant-buddy/src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import './initializeApp';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
